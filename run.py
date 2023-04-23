2202b936ce016193618f7ceed7d5d59cac2a7c8f
programminginfluencer <programminginfluencer@gmail.com>
2023-04-23 19:25:22 +0530

    Revert "added health statement"
    This reverts commit e9cd922a7db82433556b03b2eead931b7e2a7719.

--- run.py
+++ run.py
@@ -2,6 +2,5 @@ run = input("Do you run in the morning: ")
 
 if run == 'yes':
     print("You are doing great work")
-    print("It's good for your overall health")
 else:
     print("You should run in the morning")
