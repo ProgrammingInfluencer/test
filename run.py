b506d56bb5f425d515bb8ee561c0ba864383f584
programminginfluencer <programminginfluencer@gmail.com>
2023-04-27 10:00:59 +0530

    5 kilometers run statement

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ run = input("Do you run in the morning: ")
 
 if run == 'yes':
     print("You are doing great work")
+    print("Run fast and try to run 5 kilometers")
 else:
     print("You should run in the morning")
